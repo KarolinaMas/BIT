e605861993993849ba68252035c8c83855014a93
Karolina <kmkarole@yahoo.com>
2024-09-04 21:25:49 +0300

    final version

---
+++ JS/24 paskaita (07-27) Events/teorija.js
@@ -0,0 +1,9 @@
+//Events
+// trys budai kaip pridet prie elemento eventa: inline, .addEventListener(), .onclick = funkcija.
+// Butent inline variante 'event' reikia parasyti parametre.
+// event.target - grazina visa elento html koda.
+// event.target.textContent += ' + Paspaustas'; - ivykus eventui pridedamas tekstas elemente.
+// document.querySelector('.klase').computedStyleMap.display = 'block'; - galima tiesiogiai prideti css prie elemento.
+// .preventDefault() - standartinio HTML funkcionalumo sustabdymas.
+// prie window irgi galima pridet eventa, pvz., .addEventListener('scroll', fun);
+// window.scrollY - nurodo scrolinimo kordinates (reikalinga pvz pridedand prie headerio seseli kai scrolini).
