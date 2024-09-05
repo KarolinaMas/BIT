6e27392079d9d02fa0b6e5e3c1e09612b76b9e16
Karolina <kmkarole@yahoo.com>
2024-09-05 18:53:08 +0300

    final version

---
+++ JS/26 paskaita (08-01)/teorija.js
@@ -0,0 +1,13 @@
+//CRUD:
+//CREATE - Sukurti įrašą
+//READ - Peržiūrėti įrašą
+//UPDATE - Atnaujinti įrašą
+//DELETE - Ištrinti įrašą
+
+// onsubmit
+// getAtribute()
+// formData(form)
+// .entries()
+// .findIndex()
+// showList()
+// confirm()
