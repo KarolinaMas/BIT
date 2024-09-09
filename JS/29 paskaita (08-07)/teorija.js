9b5d36178084a0830cee65498c7a5092f2add203
Karolina <kmkarole@yahoo.com>
2024-09-09 20:25:49 +0300

    final version

---
+++ JS/29 paskaita (08-07)/teorija.js
@@ -0,0 +1,9 @@
+// is promiso i then funkcija grazinamas tik vienas argumentas, ne daugiau.
+// .finally() - atlieka veiksma nepriklausomai ar vykdoma .then() ar catch().
+// await ne async funkcijos viduje galima rasyti modulio faile.
+// moduli priskirt galima ir ne prie issorinio failo.
+// HTTP Query: https://domenas.com/?parametras=reiksme&sekantisparametras=1&darvienas=2...
+// 1. protokolas (https://)
+// 2. subdomenas (www)
+// 3. domenas (google.com)
+// 4.uklausos info (query) (?parametras=reiksme&sekantisparametras=1&darvienas=2...)
