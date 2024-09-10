15f1f960a3423db8f74bdff275ece0bafa4e623f
Karolina <kmkarole@yahoo.com>
2024-09-10 22:01:15 +0300

    final version

---
+++ JS/30 paskaita (08-08)/teorija.js
@@ -0,0 +1,4 @@
+// modalinis langas
+// dev tools => Application => local storage
+// Storage - podėlis.
+// Sesion info dings kaip langas bus uzdaromas ar refresinamas, local storage info siais nedings.
---
+++ JS/31 paskaita (08-09)/teorija.js
@@ -0,0 +1,3 @@
+// apend() - vis papildom elemento turini, pns kaip .innerHTML +=.
+// for(const key in objektas) {} - tik su for-in galima persukti cikla per objekto reiksmes.
+// Object.entries(objektas) - pasiverciam objekta i masyva ir galim naudot masyvo metodus.
